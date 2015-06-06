12d0197cd5af15acee9966b81bb3e51f684f92a1
Diven <qidi@mail.ustc.edu.cn>
2015-06-06 17:30:30 +0800

    longest common prefix

---
+++ LongestCommonPrefix/src/Solution.java
@@ -0,0 +1,46 @@
+
+public class Solution {
+	
+    
+    public String longestCommonPrefix(String[] strs) {
+    	
+    	if(strs == null || strs.length <= 0) {
+    		return "";
+    	}
+    	
+    	if(strs.length == 1) {
+    		return strs[0];
+    	}
+    	
+    	int subStrIndex = 1;
+    	String longestPrefix = "";
+    	
+    	while(subStrIndex <= strs[0].length()) {
+    		String tmpPrefix = strs[0].substring(0, subStrIndex);
+    		
+    		int i;
+    		for(i = 0; i < strs.length; i++) {
+    			if(strs[i].startsWith(tmpPrefix) == false) {
+    				break;
+    			}
+    		}
+    		
+    		if(i >= strs.length && tmpPrefix.length() > longestPrefix.length()) {
+    			longestPrefix = tmpPrefix;
+    		}
+    		
+    		subStrIndex++;
+    	}
+    	
+    	return longestPrefix;
+    }
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		
+		String[] strs = new String[]{};
+		
+		System.out.println(solution.longestCommonPrefix(strs));
+	}
+
+}
