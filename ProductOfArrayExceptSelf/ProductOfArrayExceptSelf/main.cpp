6366a9d5314ba0c9e15e66b0feba0741378778fa
cangjie <cangjie@mogujie.com>
2016-02-24 20:43:30 +0800

    ProductOfArrayExceptSelf

--- ProductOfArrayExceptSelf/ProductOfArrayExceptSelf/main.cpp
+++ ProductOfArrayExceptSelf/ProductOfArrayExceptSelf/main.cpp
@@ -7,9 +7,47 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace::std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+        int len = nums.size();
+        vector<int> pSeq(len);
+        vector<int> qSeq(len);
+        
+        pSeq[0] = 1;
+        for(int i = 1; i < len; i++) {
+            pSeq[i] = pSeq[i-1] * nums[i-1];
+        }
+        
+        qSeq[len-1] = 1;
+        for(int j = len-2; j >=0; j--) {
+            qSeq[j] = qSeq[j+1] * nums[j+1];
+        }
+        
+        for(int i = 0; i < len; i++) {
+            pSeq[i] *= qSeq[i];
+        }
+        
+        return pSeq;
+        
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    Solution solution;
+    vector<int> nums;
+    nums.push_back(1);
+    
+    nums.push_back(2);
+    nums.push_back(3);
+    nums.push_back(4);
+    
+    solution.productExceptSelf(nums);
     return 0;
 }
