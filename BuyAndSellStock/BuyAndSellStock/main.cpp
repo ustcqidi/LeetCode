6ee41f951ab70d66b7a907287910b6598062644f
cangjie <cangjie@mogujie.com>
2016-02-24 20:41:49 +0800

    BuyAndSellStock

--- BuyAndSellStock/BuyAndSellStock/main.cpp
+++ BuyAndSellStock/BuyAndSellStock/main.cpp
@@ -7,9 +7,54 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        if (prices.size() == 0) {
+            return 0;
+        }
+        
+        int profit = 0;
+        int buyInPrice = prices.at(0);
+
+        
+        for (int i = 1; i < prices.size(); i++) {
+            int todayPrice = prices.at(i);
+            
+            if (buyInPrice > todayPrice) {
+                buyInPrice = todayPrice;
+            } else {
+                if (i+1 < prices.size()) {
+                    int tommoryPrice = prices.at(i+1);
+                    if (todayPrice > tommoryPrice) {
+                        profit += (todayPrice - buyInPrice);
+                        buyInPrice = prices.at(i+1);
+                    }
+                } else {
+                    profit += (todayPrice - buyInPrice);
+                }
+
+            }
+        }
+        
+        return profit;
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    Solution solution;
+    
+    vector<int> prices;
+    
+    prices.push_back(2);
+    prices.push_back(1);
+    
+    std::cout << solution.maxProfit(prices);
     return 0;
 }
