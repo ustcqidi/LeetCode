a821e7e56f11f2e3d3be84535e56a1d4c66fbd76
cangjie <cangjie@mogujie.com>
2016-02-24 20:42:42 +0800

    HappyNum

--- HappyNum/HappyNum/main.cpp
+++ HappyNum/HappyNum/main.cpp
@@ -7,9 +7,44 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+        vector<int> nums;
+        
+        
+        
+        while (std::find(nums.begin(), nums.end(), n) == nums.end()) {
+            nums.push_back(n);
+            n = getSquareSum(n);
+        }
+        
+        return n == 1;
+        
+    }
+    
+    int getSquareSum(int n) {
+        int sum = 0;
+        
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit*digit;
+            
+            n = n / 10;
+        }
+        
+        return sum;
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    Solution solu;
+    std::cout << solu.isHappy(7);
     return 0;
 }
