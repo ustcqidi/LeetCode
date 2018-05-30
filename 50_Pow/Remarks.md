eee03a1944379329024fe02391a76ab5ad42d2fa
ustcqidi <568525534@qq.com>
2018-05-30 15:16:45 +0800

    add solution 50. Pow(x, n)

---
+++ 50_Pow/Remarks.md
@@ -0,0 +1,8 @@
+### Pow(x, n)
+- [题目链接](https://leetcode.com/problems/powx-n/description/)
+- 递归
+
+### 基本思路
+- 递归注意TLE
+- Your runtime beats 85.45 % of cpp submissions.
+- Runtime: 10 ms
--- 50_Pow/Solution.cpp
+++ 50_Pow/Solution.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    map<int, int> sMap;
-    
     double myPow(double x, int n) {
         if (n == 0)
             return 1;
-    
-        if (n > 0) {
-            double ret = pow(x, n);
-            return ret;
-        } else {
-            double ret = pow(x, -n);
-            return 1/ret;
-        }
-    }
-    
-    double pow(double x, int n) {
-        if (n == 0)
-            return 1;
-        
-        if (n == 1) 
+
+        if (n == 1)
             return x;
-        
-        double ret = pow(x, n/2);
-        
-        return ret*ret*(n%2?x:1);
+
+        if (n == -1)
+            return 1/x;
+
+        return myPow(x, n%2) * myPow(x*x, n/2);
     }
 };
